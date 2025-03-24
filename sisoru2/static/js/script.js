1023dcbb6fb1a60ac275205dfeb3779c9d53faeb
ManeeshK122 <maneeshkanumolu22@gmail.com>
2025-03-24 19:47:12 +0530

    Delete sisoru2 directory

--- sisoru2/static/js/script.js
+++
@@ -1,5 +0,0 @@
-function logout() {
-    fetch('/logout', { method: 'GET', credentials: 'include' })
-      .then(() => window.location.href = '../templates/login.html')
-      .catch(err => console.error('Logout error:', err));
-  }
